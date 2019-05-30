189bac8204f45ad14db22492fea9ffe64b3c8916
asdfpowdfeuavxz <43964090+asdfpowdfeuavxz@users.noreply.github.com>
2019-05-30 11:40:22 +0200

    las;mcfklasmsc
    lknxvklnxcjkvnxcjk

--- Z2Receiver.java
+++ Z2Receiver.java
@@ -1,6 +1,7 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.HashMap;
 
 public class Z2Receiver {
     static final int DATAGRAM_SIZE = 50;
@@ -9,6 +10,9 @@ public class Z2Receiver {
     DatagramSocket socket;
 
     ReceiverThread receiver = new ReceiverThread();
+    HashMap<Integer, Character> received = new HashMap<Integer, Character>();
+    int next_to_display = 0;
+    
 
     public Z2Receiver(int myPort, int destPort) throws Exception {
         localHost = InetAddress.getByName("127.0.0.1");
@@ -25,8 +29,9 @@ public class Z2Receiver {
                             new DatagramPacket(data, DATAGRAM_SIZE);
                     socket.receive(packet);
                     Z2Packet p = new Z2Packet(packet.getData());
-                    System.out.println("R:" + p.getIntAt(0)
-                            + ": " + (char) p.data[4]);
+                    if(p.getIntAt(0) >= next_to_display)
+                        received.put(Integer.valueOf(p.getIntAt(0)), Character.valueOf((char)p.data[4]));
+                    print();
                     // WYSLANIE POTWIERDZENIA
                     packet.setPort(destinationPort);
                     socket.send(packet);
@@ -35,7 +40,15 @@ public class Z2Receiver {
                 System.out.println("Z2Receiver.ReceiverThread.run: " + e);
             }
         }
-
+        
+        private void print() {
+            while(received.containsKey(Integer.valueOf(next_to_display))) {
+                System.out.println("R:" + next_to_display
+                    + ": " + received.get(Integer.valueOf(next_to_display)));
+                received.remove(Integer.valueOf(next_to_display));
+                next_to_display++;
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
--- Z2Sender.java
+++ Z2Sender.java
@@ -1,6 +1,8 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Vector;
+import java.util.Iterator;
 
 class Z2Sender {
     static final int DATAGRAM_SIZE = 50;
@@ -11,6 +13,7 @@ class Z2Sender {
     DatagramSocket socket;
     SenderThread sender = new SenderThread();
     ReceiverThread receiver = new ReceiverThread();
+    Vector<TimedDatagramPacket> sent = new Vector<TimedDatagramPacket>();
 
     public Z2Sender(int myPort, int destPort) throws Exception {
         localHost = InetAddress.getByName("127.0.0.1");
@@ -18,21 +21,77 @@ class Z2Sender {
         socket = new DatagramSocket(myPort);
     }
 
+    class TimedDatagramPacket {
+        public DatagramPacket packet;
+        public float time;
+        
+        public TimedDatagramPacket(DatagramPacket p) {
+            packet = p;
+            updateTime();
+            
+        }
+        
+        public void updateTime() {
+            time = System.currentTimeMillis();
+        }
+        
+        public int getSeq() {
+            Z2Packet p = new Z2Packet(packet.getData());
+            return p.getIntAt(0);
+        }
+        
+        public boolean isOld() {
+            float current = System.currentTimeMillis();
+            if(current - time > 10000f) {
+                return true;
+            }
+            return false;
+        }
+    }
+    
     class SenderThread extends Thread {
+        boolean eof = false;
+        int i = 0;
         public void run() {
-            int i, x;
             try {
-                for (i = 0; (x = System.in.read()) >= 0; i++) {
-                    Z2Packet p = new Z2Packet(4 + 1);
-                    p.setIntAt(i, 0);
-                    p.data[4] = (byte) x;
-                    DatagramPacket packet = new DatagramPacket(p.data, p.data.length, localHost, destinationPort);
-                    socket.send(packet);
+                while (!sent.isEmpty() || !eof) {
+                    DatagramPacket packet = next_datagrampacket();
+                    if(packet != null) {
+                        socket.send(packet);
+                    }
                     sleep(SLEEP_TIME);
                 }
             } catch (Exception e) {
                 System.out.println("Z2Sender.SenderThread.run: " + e);
             }
+        }
+        
+        private DatagramPacket next_datagrampacket() throws java.io.IOException {
+            //1. Może w sent jest coś starego
+            Iterator<TimedDatagramPacket> it = sent.iterator();
+            while (it.hasNext()) {
+                TimedDatagramPacket tmp = it.next();
+                if(tmp.isOld()) {
+                    tmp.updateTime();
+                    return tmp.packet;
+                }
+            }
+            
+            //2. Skoro nie, to sprawdzić stdin
+            if(eof) {
+                return null;
+            }
+            int x = System.in.read();
+            if(x < 0) {
+                eof = true;
+                return null;
+            }
+            Z2Packet p = new Z2Packet(4 + 1);
+            p.setIntAt(i++, 0);
+            p.data[4] = (byte)x;
+            DatagramPacket packet = new DatagramPacket(p.data, p.data.length, localHost, destinationPort);
+            sent.add(new TimedDatagramPacket(packet));
+            return packet;
         }
     }
 
@@ -44,7 +103,14 @@ class Z2Sender {
                     DatagramPacket packet = new DatagramPacket(data, DATAGRAM_SIZE);
                     socket.receive(packet);
                     Z2Packet p = new Z2Packet(packet.getData());
-                    System.out.println("S:" + p.getIntAt(0) + ": " + (char) p.data[4]);
+                    int seq = p.getIntAt(0);
+                    Iterator<TimedDatagramPacket> it = sent.iterator();
+                    while (it.hasNext()) {
+                        if(it.next().getSeq() == seq) {
+                            it.remove();
+                        }
+                    }
+                    //System.out.println("S:" + p.getIntAt(0) + ": " + (char) p.data[4]);
                 }
             } catch (Exception e) {
                 System.out.println("Z2Sender.ReceiverThread.run: " + e);
